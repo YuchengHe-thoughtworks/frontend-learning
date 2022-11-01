a4adbfdee3c24b27e194e60f34eb0a9511588cee
Freewhisper77 <emmasddx@foxmail.com>
2022-11-01 17:35:06 +0800

    complete part of calculate average&mid value

--- js/Array Practice/calculate average&mid value.js
+++ js/Array Practice/calculate average&mid value.js
@@ -1,6 +1,6 @@
 // 计算输入数组中数字元素的平均值和中值。
 const arr = [12, 46, 32, 64];
-// Task1. 计算sum 拿到数字元素个数
+// Task1. 计算sum + 拿到数字元素个数
 // Resolve: array.reduce
 function getAverage() {
   const n = arr.length + 1;
@@ -9,6 +9,9 @@ function getAverage() {
   return sum / n;
 }
 
+// Task2. sort升序排序 + 根据index拿中间的value（注意length的奇偶）
+// sort直接修改原数组
+arr.sort((a, b) => a - b); // arr = [ 12, 32, 46, 64 ]
 let averageValue = getAverage();
 let midValue;
-console.log(` mean: ${averageValue}, median: ${midValue} `);
+// console.log(` mean: ${averageValue}, median: ${midValue} `);
