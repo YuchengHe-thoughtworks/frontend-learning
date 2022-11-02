e8d78d5a7a7c34c4a2c5292821d33388831b8af2
Freewhisper77 <emmasddx@foxmail.com>
2022-11-02 09:06:14 +0800

    feat:complete the exercise of calculate average&median

--- js/Array Practice/calculate average&mid value.js
+++ js/Array Practice/calculate average&mid value.js
@@ -2,8 +2,9 @@
 const arr = [12, 46, 32, 64];
 // Task1. 计算sum + 拿到数字元素个数
 // Resolve: array.reduce
+const n = arr.length;
+// console.log(n);
 function getAverage() {
-  const n = arr.length + 1;
   const sum = arr.reduce((x, y) => x + y);
 
   return sum / n;
@@ -12,6 +13,16 @@ function getAverage() {
 // Task2. sort升序排序 + 根据index拿中间的value（注意length的奇偶）
 // sort直接修改原数组
 arr.sort((a, b) => a - b); // arr = [ 12, 32, 46, 64 ]
+
+function getMid() {
+  if (n % 2 === 0) {
+    return (arr[n/2 - 1] + arr[n/2])/2;
+  }else {
+    // Math.floor()向下取整
+    return arr[Math.floor(n/2)];
+  }
+}
+
 let averageValue = getAverage();
-let midValue;
-// console.log(` mean: ${averageValue}, median: ${midValue} `);
+let midValue = getMid();
+console.log(` mean: ${averageValue}, median: ${midValue} `);
