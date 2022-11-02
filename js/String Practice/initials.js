1bf7aead7c7b8ddc5373841ec9eeba7de09f5993
Freewhisper77 <emmasddx@foxmail.com>
2022-11-02 15:08:20 +0800

    refactor:last exercise

--- js/String Practice/initials.js
+++ js/String Practice/initials.js
@@ -2,9 +2,14 @@
 // 给定的输入是一个由多个单词组成的字符串，每个单词之间有一个空格。
 // 缩写名称并返回名称首字母。
 const input = 'George Raymond Richard Martin';
+// console.log(input[0]); // G
 const arr = input.split(' '); // [ 'George', 'Raymond', 'Richard', 'Martin' ]
 console.log(arr.map(getInitial).join(''));
 
+// function getInitial(string) {
+//   return string.split('', 1).join();
+// }
+
 function getInitial(array) {
-  return array.split('', 1).join();
+  return array[0];
 }
