b444345bb8d154c6296b17b600ebd8759c6a3bd6
Freewhisper77 <emmasddx@foxmail.com>
2022-11-02 14:41:03 +0800

    feat: complete the exercise of get the first letter of a string

---
+++ js/String Practice/initials.js
@@ -0,0 +1,10 @@
+// 【获取姓名缩写】
+// 给定的输入是一个由多个单词组成的字符串，每个单词之间有一个空格。
+// 缩写名称并返回名称首字母。
+const input = 'George Raymond Richard Martin';
+const arr = input.split(' '); // [ 'George', 'Raymond', 'Richard', 'Martin' ]
+console.log(arr.map(getInitial).join(''));
+
+function getInitial(array) {
+  return array.split('', 1).join();
+}
