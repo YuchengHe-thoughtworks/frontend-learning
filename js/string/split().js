503db4ff1b0e04c47247c1c16d315e5a432ed9cc
Freewhisper77 <emmasddx@foxmail.com>
2022-11-02 14:38:47 +0800

    learn: String.split

--- js/string/split().js
+++ js/string/split().js
@@ -1,11 +1,17 @@
 const words = 'what a beautiful    day';
 
-// 3. split()方法将String分为有序的子字符串列表，
+// split()方法将String分为有序的子字符串列表，
 // 将这些子字符串放入数组中，然后【返回数组】。
-const splitted = words.split('a');
-const splitted1 = words.split(' ')
+const splitted = words.split(' ');
+const splitted1 = words.split('a', 4); // array
+const splitted2 = words.split(' ') // string
   .join();
 
-console.log(typeof splitted); // array
-console.log(splitted); // object
-console.log(splitted1);
+const n1 = splitted[2]; // beautiful
+const n2 = n1.split('');
+console.log(n2[2]); // a
+
+const strCopy = words.split(); // [ 'what a beautiful    day' ]
+
+// Params1: separator 可以是一个字符串OR正则表达式
+// Params2: limit 是一个整数，限定返回的分割片段数量。
